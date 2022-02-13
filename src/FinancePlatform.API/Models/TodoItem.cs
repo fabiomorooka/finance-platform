05572f0fe3d73aeec7f8e41f91fce8f2a965ce79
fabio.morooka <fabio.morooka@gmail.com>
2022-02-13 14:58:07 -0300

    insert initial DTO

--- src/FinancePlatform.API/Models/TodoItem.cs
+++ src/FinancePlatform.API/Models/TodoItem.cs
@@ -5,5 +5,6 @@
         public long Id { get; set; } //unique key in a relational database.
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
     }
 }
