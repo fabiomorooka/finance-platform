69858066b85eafbd4e6ffbba28abded6dc650f69
fabio.morooka <fabio.morooka@gmail.com>
2022-02-13 15:13:24 -0300

    update swagger in program file

--- src/FinancePlatform.API/Program.cs
+++ src/FinancePlatform.API/Program.cs
@@ -8,14 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("FinancePlatform"));
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new() { Title = "FinancePlatform", Version = "v1" });
-//});
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "FinancePlatform", Version = "v1" });
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
